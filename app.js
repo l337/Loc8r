9455d3d639ee3bddebcf05cc970d7f495fcfcaf7
l337 <autonomousmachine@gmail.com>
2015-01-30 17:49:21 -0800

    initial commit to the Loc8r proj

---
+++ app.js
@@ -0,0 +1,47 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , http = require('http');
+
+
+var app = express();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/app_server/views');
+  app.set('view engine', 'jade');
+  //app.use(express.bodyParser());
+
+  app.use(express.json());
+  app.use(express.urlencoded());
+
+  app.use(express.methodOverride());
+  app.use(express.cookieParser());
+  app.use(express.session({ secret: 'your secret here' }));
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+// Routes
+
+require('./routes')(app);
+
+http.createServer(app).listen(3000);
+
+/*
+app.listen(3000, function(){
+  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+});
+*/
